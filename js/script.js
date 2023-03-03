c8baeaf9ec0937d981420628b8cf8673b95a1282
tienbot1 <tien.ru+1@mail.ru>
2023-03-03 11:21:26 +0900

    add js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ let burger = document.querySelector('.burger');
 let close = document.querySelector('.close');
 
 burger.onclick = function() {
-    // console.log('ok');
     document.querySelector('.navigation').classList.add('navigation__open');
 };
 
 close.onclick = function() {
-    // console.log('ko');
     document.querySelector('.navigation').classList.remove('navigation__open');
 };
